ea620cf0f6c76b58053de7529e171695b64f1989
Patricio Avigliano <patricioavigliano@gmail.com>
2018-12-05 14:29:15 -0300

    que se io

--- src/server.rs
+++ src/server.rs
@@ -23,9 +23,9 @@ impl Server {
     pub fn run(&self, rx: Receiver<Images>) {
         let rx_ref = &rx;
         loop {
-            info!("Server {} about to receive...", self.id);
+            //info!("Server {} about to receive...", self.id);
             let images = self.receive_request(rx_ref);
-            info!("Server {} received {:?}", self.id, images.quads);
+            info!("Server {} received {:?} from obs {}", images.srv_id, images.quads, images.obs_id);
             //sleeps velocity_of_processing secs!
             self.process_quads(images.quads);
             self.send_results(images.obs_id);
@@ -34,9 +34,9 @@ impl Server {
 
     fn process_quads(&self, q: u64) {
         let secs = time::Duration::from_millis(1000 * self.velocity_of_processing  as u64 * q);
-        info!("Server {} sleeping {:?}", self.id, secs);
+        //info!("Server {} sleeping {:?}", self.id, secs);
         thread::sleep(secs);
-        info!("Server {} ends sleeping", self.id);
+        //info!("Server {} ends sleeping", self.id);
     }
 
     fn receive_request(&self, rx: &Receiver<Images>) -> Images {
@@ -44,6 +44,7 @@ impl Server {
     }
 
     fn send_results(&self, id : u16) {
+        info!("Server {} respond to obs {}", self.id, &id);
         self.observatories[&id].send(Results{found : 0, srv_id: self.id}).unwrap();
     }
 }
--- src/telescope.rs
+++ src/telescope.rs
@@ -1,32 +1,34 @@
 extern crate log;
 
 use std::{thread,time};
+use std::sync::mpsc::Sender;
 use images::Images;
 
 pub struct Telescope {
     velocity_of_shooting: f64,
-    number_of_quadrants: u16,
     quads_division: Vec<f64>,
     id: u16,
+    servers: Vec<Sender<Images>>,
 }
 
 impl Telescope {
-    pub fn new(vel: f64, quads: u16, id: u16, quads_division: Vec<f64>) -> Telescope {
+    pub fn new(vel: f64, quads: u16, id: u16, quads_division: Vec<f64>, servers: Vec<Sender<Images>>) -> Telescope {
         info!("Telescope created: id {} vel: {} quads: {}", id, vel, quads);
 
         Telescope {
             velocity_of_shooting: vel,
-            number_of_quadrants: quads,
             quads_division: quads_division,
             id : id,
+            servers: servers,
+
         }
     }
     pub fn take_images(&self) -> Vec<Images> {
-        let mut v = Vec::with_capacity(self.number_of_quadrants as usize);
+        let mut v = Vec::with_capacity(self.quads_division.len() as usize);
         let mut id = 0;
         //sleep velocity_of_shooting secs!
         let secs = time::Duration::from_millis(1000*self.velocity_of_shooting as u64);
-        info!("Telescope {} sleeping {:?}", self.id, secs);
+        //info!("Telescope {} sleeping {:?}", self.id, secs);
         thread::sleep(secs);
         for total_quads in self.quads_division.clone() {
             v.push(Images {
@@ -38,4 +40,17 @@ impl Telescope {
         }
         v
     }
+
+    pub fn send_images_to_servers(&self) {
+        loop{
+            let images : Vec<Images> = self.take_images();
+            for image in images {
+                let tx = self.servers[image.srv_id as usize].clone();
+                //info!("Obs {} about to send...", self.id);
+                info!("Obs {} sent {} to server {}", self.id, image.quads, image.srv_id);
+                tx.send(image).unwrap();
+            }
+        }
+    }
+
 }
